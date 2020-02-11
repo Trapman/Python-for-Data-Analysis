d8c82dd1f6ce558d4275001401f48600c7e58722
Trapman <david.a.shuttleworth@gmail.com>
2020-02-10 21:30:09 -0500

    Create CH5 Pandas.py

---
+++ CH5 Pandas.py
@@ -0,0 +1,9 @@
+# GETTING STARTED WITH PANDAS #######################################
+import pandas as pd
+
+from pandas import Series, DataFrame
+
+# INTRO TO PANDAS DATA STRUCTURES #####################################
+# Series
+# one-dimensional array-like object containing a sequence of values and an associated array of data labels, called its index
+obj = pd.Series([4, 7, -5, 3])
